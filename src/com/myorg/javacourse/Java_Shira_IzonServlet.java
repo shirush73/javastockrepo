3f517f126a8cac26b2ff34122e1e0f9e79bab181
shirush <shirush@Shira>
2015-03-31 00:31:58 +0300

    Ex02

--- src/com/myorg/javacourse/Java_Shira_IzonServlet.java
+++ src/com/myorg/javacourse/Java_Shira_IzonServlet.java
@@ -5,7 +5,19 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class Java_Shira_IzonServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		int num1;
+		int num2;
+		int num3;
+		int result;
+		
+		num1= 4;
+		num2= 3;
+		num3= 7;
+		
+		result = (num1 + num2)* num3;
+		
+		String resultStr = new String("<h1>Result of " +num1+ "+" +num2+ " *" +num3+ "=" + result+"</h1>");
+		resp.getWriter().println(resultStr);
 	}
 }
