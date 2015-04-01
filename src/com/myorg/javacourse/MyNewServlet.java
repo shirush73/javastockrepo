f3d7d400954013b94eb0cb71f3ef7957c8580cf9
shirush <shirush@Shira>
2015-04-02 01:44:04 +0300

    final EX03

--- src/com/myorg/javacourse/MyNewServlet.java
+++ src/com/myorg/javacourse/MyNewServlet.java
@@ -12,8 +12,39 @@ public class MyNewServlet extends HttpServlet{
 			throws ServletException, IOException {
 		
 		resp.setContentType("text/html");
-		resp.getWriter().println("Hello my new Servlet");
+		
+		//Working assumptions - when an integer was given -i left it as is but for angles and area data i declared them as double
+		
+		int radius;
+		double area;
+		radius = 50;
+		area = radius * radius *Math.PI;
+		
+		String line1 = new String("calculation 1: Area of circle with radius "+radius+ " cm is "+area+" cm^2");
+		
+		int hypotenuse; // assuming the hypotenuse is fixed length
+		double angleB, opposite;
+		
+		hypotenuse = 50;
+		angleB = 30;
+		opposite = hypotenuse * Math.sin(Math.toRadians(angleB));
+		String line2 = new String("calculation 2: Length of opposite where angle B is "+angleB+" degrees is "+opposite+" cm");
+		
+		int base, exp;
+		long result; //assuming the result is a long number
+		
+		base = 20;
+		exp = 13;
+		result = (long)(Math.pow(base, exp));
+		
+		
+		String line3 = new String("calculation 3: Power of "+base+" with exp of "+exp+" is "+result);
+		
+		String resultStr = line1 + "<br>" + line2 + "<br>" + line3;
+		resp.getWriter().println(resultStr);
 	}
+		
+	
 
 }
 
