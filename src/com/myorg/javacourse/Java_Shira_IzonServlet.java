77c2699ca1844784cf0910a2b5be5f51ec101d98
shirush <shirush@Shira>
2015-04-02 01:44:04 +0300

    final Ex02

--- src/com/myorg/javacourse/Java_Shira_IzonServlet.java
+++ src/com/myorg/javacourse/Java_Shira_IzonServlet.java
@@ -17,7 +17,7 @@ public class Java_Shira_IzonServlet extends HttpServlet {
 		
 		result = (num1 + num2)* num3;
 		
-		String resultStr = new String("<h1>Result of " +num1+ "+" +num2+ " *" +num3+ "=" + result+"</h1>");
+		String resultStr = new String("<h1>Result of ( " +num1+ "+" +num2+ " ) * " +num3+ "=" + result+"</h1>");
 		resp.getWriter().println(resultStr);
 	}
 }
